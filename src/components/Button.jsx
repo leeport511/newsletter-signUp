63544de65ed373edb781a96531d06fba2493634f
leeport511 <leonardoandres.portillo@gmail.com>
2023-07-03 17:49:02 -0300

    changue button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,6 +10,7 @@ const Button = ({ children, handleSubmit, isValid }) => {
 
 
     return (
+        <>
         <button
             className="button-shadow ease-in-out w-[100%] delay-75 rounded-lg bg-dark-slate-grey px-6 py-3 text-center text-sm font-semibold tracking-wide text-white duration-500"
             type="submit"
@@ -18,6 +19,7 @@ const Button = ({ children, handleSubmit, isValid }) => {
         >
             {children}
         </button>
+        </>
     );
 };
 
